f7968d807264d0beebccb492a56e74a21fef3d9e
vseolog <150056173+vseolog@users.noreply.github.com>
2024-03-02 16:05:18 +0000

    Update wave.gd

--- wave.gd
+++ wave.gd
@@ -13,7 +13,7 @@ func _ready():
 	for i in range(1, number_of_points+1):
 		var x_position = i*(screen_leight/number_of_points)
 		add_point(Vector2(x_position, y_position), i)
-		var a = point.new(i)
+		var a = point.new(i) 
 		array.append(a)
 
 
@@ -23,7 +23,7 @@ func _physics_process(delta):
 			j.h = get_point_position(j.id).y
 			var p = array[j.id-1]
 			var n = array[j.id+1]
-			j.v = ((p.h - j.h) + (n.h - j.h)) * delta*50
+			j.v = ((p.h - j.h) + (n.h - j.h)) * delta*500
 		if j.v != 0:
 			set_point_position(j.id, Vector2(get_point_position(j.id).x, (j.h + (j.v * delta))))
 
@@ -32,8 +32,7 @@ func _input(event):
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
 		var event_x_position = float(event.get_position().x)
 		var event_y_position = float(event.get_position().y)
-		print('wave created')
-		print(event_x_position)
 		var event_point_index = int((event_x_position)/((screen_leight)/(number_of_points)))
-		print('Index of point: '+ str(event_point_index))
-		set_point_position(event_point_index, Vector2(event_x_position, event_y_position-200))
+		
+		if event_x_position < screen_leight and event_point_index > 0 and event_point_index < len(array)-1:
+			set_point_position(event_point_index, Vector2(event_x_position, event_y_position-200))
