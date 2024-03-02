b596c6357713ce405a2dc0cd54ec323e2e0267de
vseolog <150056173+vseolog@users.noreply.github.com>
2024-03-02 15:08:22 +0000

    Update point.gd

--- point.gd
+++ point.gd
@@ -1,4 +1,4 @@
-extends Line2D
+extends "res://Wave.gd"
 
 var id
 var h
@@ -6,9 +6,9 @@ var v = 0
 
 func _init(_id):
 	id = _id
-	h = get_point_position(id).y
 
-func _process(delta):
-	h = get_point_position(id).y
+func _physics_process(delta):
+	h = get_point_position(id).y + 200
+	
 	if v != 0:
-		set_point_position(id, Vector2(get_point_position(id).x, (h+v)))
+		set_point_position(id, Vector2(get_point_position(id).x, (h+v*delta)))
